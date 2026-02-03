7f6ae451023d328a4e5426d558ca70d6a61cff05
batoolshbool <batoolmoe201@gmail.com>
2026-02-03 12:03:45 +0300

    Feb 3 12:03

--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: bshbool <bshbool@student.42amman.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 09:07:17 by bshbool           #+#    #+#             */
-/*   Updated: 2026/02/03 10:05:06 by bshbool          ###   ########.fr       */
+/*   Updated: 2026/02/03 11:56:59 by bshbool          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,31 @@
 #include <unistd.h>
 #include <sys/time.h>
 #include <pthread.h>
+typedef struct s_philo;
+
+typedef struct s_table
+{
+    unsigned int nb_philo;
+    unsigned long die_time;
+    unsigned long eat_time;
+    unsigned long sleep_time;
+	unsigned long start_time;
+    int must_eat;
+    int died_end;
+    pthread_mutex_t *forks;
+	s_philo	philos;
+} t_table;
 
 typedef struct s_philo
 {
-	int		id;
-	
+	pthread_t	thread;
+	unsigned int		id;
+	unsigned int		meals;
+	unsigned int		since_last_meal;
+	pthread_mutex_t*left_fork;
+	pthread_mutex_t	*right_fork;
+	t_table	*rules;
+
 }	t_philo;
 
 
