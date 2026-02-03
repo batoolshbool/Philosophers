890d62f5fb6b68b879e7ad595d125a82c69cb1c9
batoolshbool <batoolmoe201@gmail.com>
2026-02-03 11:35:56 +0300

    Feb 3 11:35

---
+++ philo/philo.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bshbool <bshbool@student.42amman.com>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/03 09:07:17 by bshbool           #+#    #+#             */
+/*   Updated: 2026/02/03 10:05:06 by bshbool          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+typedef struct s_philo
+{
+	int		id;
+	
+}	t_philo;
+
+
+#endif
+
